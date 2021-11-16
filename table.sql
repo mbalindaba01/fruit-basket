8eb9b1b964ebb889537808043fdda61894e326ba
Mbalenhle <mbalindaba01@gmail.com>
2021-11-16 12:31:57 +0200

    fixed typo in tables file

--- table.sql
+++ table.sql
@@ -1,8 +1,8 @@
-create table Fruit_Basket (
+create table fruit_basket (
     basket_id serial not null primary key,
     fruit_type text not null,
     fruit_quantity integer not null,
     fruit_price decimal(5,2)
 )
 
-insert into Fruit_Basket (fruit_type, fruit_quantity, fruit_price) values ('Apple', 1 , '2.50'), ('Orange', 1, '3.00'), ('Banana', 1, '2.00')
+insert into fruit_basket (fruit_type, fruit_quantity, fruit_price) values ('Apple', 1 , '2.50'), ('Orange', 1, '3.00'), ('Banana', 1, '2.00')
