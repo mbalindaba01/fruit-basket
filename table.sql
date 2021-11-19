f11af4344a8708e8b5b2884a8115e725a3f13dd2
Mbalenhle <mbalindaba01@gmail.com>
2021-11-19 12:57:01 +0200

    fixed table file

--- table.sql
+++ table.sql
@@ -3,6 +3,6 @@ create table fruit_basket (
     fruit_type text not null,
     fruit_quantity integer not null,
     fruit_price decimal(5,2)
-)
+);
 
-insert into fruit_basket (fruit_type, fruit_quantity, fruit_price) values ('Apple', 1 , '2.50'), ('Orange', 1, '3.00'), ('Banana', 1, '2.00')
+insert into fruit_basket (fruit_type, fruit_quantity, fruit_price) values ('Apple', 1 , '2.50'), ('Orange', 1, '3.00'), ('Banana', 1, '2.00');
